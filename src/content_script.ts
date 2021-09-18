e289175f5d671a86740f018e1c7139373b53a5ac
Masaya Kazama <miyan@t-p.jp>
2021-09-19 03:07:51 +0900

    documentのfocusを監視するように変更

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,48 +1,38 @@
-import { selectElement } from "./lib/selector";
 import {
   getSearchTimeText,
-  getTweetId,
-  getUserId,
-  isUserPage,
-  tweetId2Time,
+  getTweetInfo,
+  isSearchInputElement,
+  TweetInfo,
 } from "./lib/twitter";
 
-const getInputText = (userId: string, tweetId?: string) => {
-  const from = `from:${userId}`;
-  if (!tweetId) return from;
-  const tweetTime = tweetId2Time(tweetId);
+const getInputQuery = (info: TweetInfo) => {
+  const from = { from: info.userId };
+  if (info.type === "account") return from;
   // add 1sec
-  const searchTimeText = getSearchTimeText(tweetTime + 1000);
-  return `${from} until:${searchTimeText}`;
+  const searchTimeText = getSearchTimeText(info.time + 1000);
+  return { ...from, until: searchTimeText };
 };
 
-const onTitleChange = async () => {
-  const tweetId = getTweetId();
-  if (!(tweetId || isUserPage())) return;
-  const userId = getUserId();
-  const input = await selectElement<HTMLInputElement>(
-    `[data-testid="SearchBox_Search_Input"]`
-  );
-  if (!userId || !input) return;
+const onFocus = (evt: Event) => {
+  const target = evt.target;
+  if (!(target && isSearchInputElement(target))) return;
+
+  const info = getTweetInfo();
+  if (!info) return;
 
   const text =
-    getInputText(userId, tweetId) + " -filter:replies include:nativeretweets ";
+    Object.entries(getInputQuery(info))
+      .map(([key, val]) => `${key}:${val}`)
+      .join(" ") + " "; //+ " -filter:replies include:nativeretweets ";
 
-  input.addEventListener("focus", () => {
-    requestAnimationFrame(() => {
-      document.execCommand("insertText", false, text);
-      input.select();
-    });
+  requestAnimationFrame(() => {
+    target.value = "";
+    document.execCommand("insertText", false, text);
+    target.select();
   });
 };
 
-const init = async () => {
-  const title = await selectElement<HTMLTitleElement>("title");
-  if (!title) return;
-  // ページ遷移を拾うためtitle要素の変更を監視する
-  const observer = new MutationObserver(onTitleChange);
-  observer.observe(title, {
-    childList: true,
-  });
+const init = () => {
+  document.addEventListener("focus", onFocus, true);
 };
 init();
--- src/lib/twitter.ts
+++ src/lib/twitter.ts
@@ -4,12 +4,12 @@ export const isUserPage = () => {
 
 const statusReg = new RegExp("/[^/]+/status/(?<id>\\d+)");
 
-export const getTweetId = () => {
-  return location.pathname.match(statusReg)?.groups?.id;
+export const getStatusId = (pathname: string) => {
+  return pathname.match(statusReg)?.groups?.id;
 };
 
-export const getUserId = () => {
-  return location.pathname.match(/^\/(?<id>[^/]+).*/)?.groups?.id;
+export const getUserId = (pathname: string) => {
+  return pathname.match(/^\/(?<id>[^/]+).*/)?.groups?.id;
 };
 
 export const tweetId2Time = (id: string) => {
@@ -19,6 +19,48 @@ export const tweetId2Time = (id: string) => {
 };
 
 export const getSearchTimeText = (time: number) => {
-  // '2021-09-17T18:49:03.304Z' -> '2021-09-17_18:49:03_UTC'
+  // '2000-01-23T01:23:45.678Z' -> '2000-01-23_01:23:45_UTC'
   return new Date(time).toISOString().replace(/(.+?)T(.+?)\..+/, "$1_$2_UTC");
+};
+
+export type TweetInfo =
+  | {
+      type: "account";
+      userId: string;
+    }
+  | {
+      type: "status";
+      userId: string;
+      statusId: string;
+      time: number;
+    };
+export const getTweetInfo = (): TweetInfo | null => {
+  const { pathname } = location;
+  const statusId = getStatusId(pathname);
+
+  if (!(statusId || isUserPage())) return null;
+  const userId = getUserId(pathname);
+
+  if (!userId) return null;
+
+  return statusId
+    ? {
+        type: "status",
+        userId,
+        statusId,
+        time: tweetId2Time(statusId),
+      }
+    : {
+        type: "account",
+        userId,
+      };
+};
+
+export const isSearchInputElement = (
+  target: EventTarget
+): target is HTMLInputElement => {
+  return (
+    target instanceof HTMLInputElement &&
+    target.dataset.testid === "SearchBox_Search_Input"
+  );
 };
